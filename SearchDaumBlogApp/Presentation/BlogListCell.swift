ad7c31ac1ff58f48d01755a8222bb9a460b5e4e3
최지연 <choi02647@naver.com>
2023-05-30 15:47:23 +0900

    폴더 이동

---
+++ SearchDaumBlogApp/Presentation/BlogListCell.swift
@@ -0,0 +1,12 @@
+//
+//  BlogListCell.swift
+//  SearchDaumBlogApp
+//
+//  Created by 최지연/클라이언트 on 2023/05/30.
+//
+
+import UIKit
+import SnapKit
+import Kingfisher
+
+class BlogListCell: UITableViewCell {}
