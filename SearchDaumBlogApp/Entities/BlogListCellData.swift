e52ddf88e3fccf00066ee26353b7807e37fcc4a3
최지연 <choi02647@naver.com>
2023-05-30 15:52:12 +0900

    added BlogListCell, BlogListCellData

---
+++ SearchDaumBlogApp/Entities/BlogListCellData.swift
@@ -0,0 +1,15 @@
+//
+//  BlogListCellData.swift
+//  SearchDaumBlogApp
+//
+//  Created by 최지연/클라이언트 on 2023/05/30.
+//
+
+import Foundation
+
+struct BlogListCellData {
+    let thumbnailURL: URL?
+    let name: String?
+    let title: String?
+    let datetime: Date?
+}
