882d3b193bab4f75dceb21061a4ae1b0e623c837
최지연 <choi02647@naver.com>
2023-05-30 16:19:42 +0900

    added alert

--- SearchDaumBlogApp/Presentation/MainViewController.swift
+++ SearchDaumBlogApp/Presentation/MainViewController.swift
@@ -12,6 +12,11 @@ import RxCocoa
 class MainViewController: UIViewController {
     let disposeBag = DisposeBag()
     
+    let listView = BlogListView()
+    let searchBar = SearchBar()
+    
+    let alertActionTapped = PublishRelay<AlertAction>()
+    
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super .init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         
@@ -24,9 +29,86 @@ class MainViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func bind() {}
+    private func bind() {
+        let alertSheetForSorting = listView.headerView.sortButtonTapped
+            .map { _ -> Alert in
+                return (title: nil, message: nil, actions: [.title, .datetime, .cancel], style: .actionSheet)
+            }
+    }
     
-    private func attribute() {}
+    private func attribute() {
+        title = "다음 블로그 검색"
+        view.backgroundColor = .white
+    }
+    
+    private func layout() {
+        [searchBar, listView].forEach { view.addSubview($0) }
+        
+        searchBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+        }
+        
+        listView.snp.makeConstraints {
+            $0.top.equalTo(searchBar.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+}
 
-    private func layout() {}
+// Alert
+extension MainViewController {
+    typealias Alert = (title: String?, message: String?, actions: [AlertAction], style: UIAlertController.Style)
+
+    enum AlertAction: AlertActionConvertible {
+        case title, datetime, cancel
+        case confirm
+        
+        var title: String {
+            switch self {
+            case .title:
+                return "Title"
+            case .datetime:
+                return "Datetime"
+            case .cancel:
+                return "취소"
+            case .confirm:
+                return "확인"
+            }
+        }
+        
+        var style: UIAlertAction.Style {
+            switch self {
+            case .title, .datetime:
+                return .default
+            case .cancel, .confirm:
+                return .cancel
+            }
+        }
+    }
+    
+    func presentAlertController<Action: AlertActionConvertible>(_ alertController: UIAlertController, actions: [Action]) -> Signal<Action> {
+        if actions.isEmpty { return .empty() }
+        return Observable
+            .create { [unowned self] observer in
+                for action in actions {
+                    alertController.addAction(
+                        UIAlertAction(
+                            title: action.title,
+                            style: action.style,
+                            handler: { _ in
+                                observer.onNext(action)
+                                observer.onCompleted()
+                            }
+                        )
+                    )
+                }
+                self.present(alertController, animated: true, completion: nil)
+                
+                return Disposables.create {
+                    alertController.dismiss(animated: true, completion: nil)
+                }
+            }
+            .asSignal(onErrorSignalWith: .empty())
+        }
 }
