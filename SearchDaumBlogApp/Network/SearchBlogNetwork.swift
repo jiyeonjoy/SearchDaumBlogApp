9f14c65ef648ca684347a8a96d68d6bc7e242e46
최지연 <choi02647@naver.com>
2023-05-30 16:38:49 +0900

    added SearchBlogNetwork

---
+++ SearchDaumBlogApp/Network/SearchBlogNetwork.swift
@@ -0,0 +1,62 @@
+//
+//  SearchBlogNetwork.swift
+//  SearchDaumBlogApp
+//
+//  Created by 최지연/클라이언트 on 2023/05/30.
+//
+
+import RxSwift
+import Foundation
+
+struct SearchBlogApi {
+    static let scheme = "https"
+    static let host = "dapi.kakao.com"
+    static let path = "/v2/search/"
+    
+    func searchBlog(query: String) -> URLComponents {
+        var components = URLComponents()
+        components.scheme = SearchBlogApi.scheme
+        components.host = SearchBlogApi.host
+        components.path = SearchBlogApi.path + "blog"
+        
+        components.queryItems = [
+            URLQueryItem(name: "query", value: query),
+            URLQueryItem(name: "size", value: "25")
+        ]
+        
+        return components
+    }
+}
+
+class SearchBlogNetwork {
+    private let session: URLSession
+    let api = SearchBlogApi()
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func searchBlog(query: String) -> Single<Result<DKBlog, SearchNetworkError>> {
+        guard let url = api.searchBlog(query: query).url else {
+            return .just(.failure(.invalidURL))
+        }
+        
+        let request = NSMutableURLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("KakaoAK 4e78ea35cffb481201121cd3d09455a6", forHTTPHeaderField: "Authorization") // 헤더 추가
+        
+        return session.rx.data(request: request as URLRequest)
+            .map { data in
+                do {
+                    let blogData = try JSONDecoder().decode(DKBlog.self, from: data)
+                    return .success(blogData)
+                } catch {
+                    return .failure(.invalidJSON)
+                }
+            }
+            .catch { _ in
+                .just(.failure(.networkError)) // 에러 발생 시 네트워크 에러 발생!
+            }
+            .asSingle()
+    }
+}
