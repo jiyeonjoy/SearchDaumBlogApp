18cb2248ab694721e4a2a63cf38301bdc2af9c56
최지연 <choi02647@naver.com>
2023-05-30 15:17:08 +0900

    MainViewController 구조 잡기

--- SearchDaumBlogApp/MainViewController.swift
+++ SearchDaumBlogApp/MainViewController.swift
@@ -6,6 +6,27 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class MainViewController: UIViewController {
+    let disposeBag = DisposeBag()
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super .init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        bind()
+        attribute()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func bind() {}
+    
+    private func attribute() {}
+
+    private func layout() {}
 }
